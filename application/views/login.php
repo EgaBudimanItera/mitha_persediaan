873b5a7c81fedb83f301dad937eb50b9a0202ca3
v3nd3tt4 <pilopaokta@gmail.com>
2018-06-19 10:46:27 +0700

    new template login

--- application/views/login.php
+++ application/views/login.php
@@ -11,9 +11,48 @@
 	    <link rel="stylesheet" href="<?=base_url()?>assets/back-end/assets/plugins/bootstrap/css/bootstrap.min.css">
 		<link rel="stylesheet" href="<?=base_url()?>assets/back-end/assets/plugins/font-awesome/css/font-awesome.min.css">
 	</head>
-	<body>
+	<body style="background-color: #E2E2E2;">
 		<br/><br/><br/>
+
 		<div class="container">
+        <div class="row text-center " style="padding-top:100px;">
+            <div class="col-md-12">
+                <h3>Aplikasi Stok Barang</h3>
+            </div>
+        </div>
+         <div class="row ">
+               
+                <div class="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1">
+                           
+                    <div class="panel-body">
+                        <form action="<?=base_url()?>c_login/proses_login" method="POST" id="login">
+                            <hr />
+                            
+                             <div class="form-group input-group">
+                                    <span class="input-group-addon"><i class="fa fa-tag"  ></i></span>
+                                    <input id="username" type="text" class="form-control" name="username" placeholder="Username" required />
+                                </div>
+                            <div class="form-group input-group">
+                                    <span class="input-group-addon"><i class="fa fa-lock"  ></i></span>
+                                    <input id="password" type="password" class="form-control" name="password" placeholder="Password" required />
+                                </div>
+                            
+                             
+                             <div class="form-group">
+			                    <button type="submit" class="btn btn-primary" data-toggle="modal" data-target="#myModal">Login</button>
+			                    Halaman utama, klik <a href="<?=base_url()?>">disini</a>
+			                </div>
+                            
+                            </form>
+                    </div>
+                   
+                </div>
+                
+                
+        </div>
+    </div>
+
+		<!-- <div class="container">
 		    <div class="row">
 		        <div class="col-md-offset-4 col-md-4 login-from" style="border:solid thin #eae7de">
 		            <h4>Aplikasi Stok Barang</h4><hr/>
@@ -36,7 +75,7 @@
 		            <br />     
 		        </div>
 		    </div>
-		</div> 
+		</div>  -->
 		<script src="<?=base_url()?>assets/back-end/assets/plugins/jquery-2.1.0.min.js"></script>
   <script src="<?=base_url()?>assets/back-end/assets/plugins/bootstrap/js/bootstrap.min.js"></script>
 	</body>
